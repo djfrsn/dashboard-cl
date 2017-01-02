cb12c0278f2668efaa0038e71102ee0d92507952
Dennis Jefferson <dennismjefferson@gmail.com>
2017-01-02 14:10:58 -0800

    use single quotes

--- src/core/firebase/config.js
+++ src/core/firebase/config.js
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBH_jCVn_6LgGELyJJEhXjP8O_A-Io0qZ0",
-  authDomain: "dashboard-cli.firebaseapp.com",
-  databaseURL: "https://dashboard-cli.firebaseio.com",
-  storageBucket: "dashboard-cli.appspot.com",
+  apiKey: 'AIzaSyBH_jCVn_6LgGELyJJEhXjP8O_A-Io0qZ0',
+  authDomain: 'dashboard-cli.firebaseapp.com',
+  databaseURL: 'https://dashboard-cli.firebaseio.com',
+  storageBucket: 'dashboard-cli.appspot.com',
   messagingSenderId: "50706334069"
 };
